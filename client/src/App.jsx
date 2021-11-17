ec7a930dc55096672a62251743b2e83883104bc5
Caleb Broderick <liquidarkness1997@gmail.com>
2021-11-17 16:21:08 -0700

    setup html structure and states

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [hideAddCoin, setHideAddCoin] = useState(false);
+
+  const [coinName, setCoinName] = useState('');
+  const [dateFieldName, setDateFieldName] = useState('');
+  const [coinAmountFieldName, setCoinAmountFieldName] = useState('');
+  const [isUnixField, setIsUnixField] = useState(false)
+
+  const handleInputChange = (e) => {
+    switch(e.target.id) {
+      case 'nameField':
+        setCoinName(e.target.value);
+        break;
+      case 'dateField':
+        setDateFieldName(e.target.value);
+        break;
+      case 'amountField':
+        setCoinAmountFieldName(e.target.value);
+        break;
+      case 'isUnix':
+        if (e.target.value === 'no') {
+          setIsUnixField(false);
+        } else {
+          setIsUnixField(true);
+        }
+        break;
+    }
+  }
+
   return (
     <div>
-      <h1>Hello coolio world?</h1>
+      <h1>Crypto Csv Info</h1>
+      <div>
+        <div style={{fontSize: '1.5rem', margin: '0 0 0.5rem 0.5rem'}}>
+          <label htmlFor='cryptoCoins' style={{marginRight: '3rem'}}>Select Crypto Coin</label>
+          <select name='cryptoCoins' id='cryptoCoins'>
+          </select>
+        </div>
+        <div style={{fontSize: '1.5rem', margin: '0 0 0.5rem 0.5rem'}}>
+          <label htmlFor='csvFile' style={{marginRight: '1.7rem'}}>Select CSV file</label>
+          <input type='file' id='csvFile' name='csvFile'></input>
+        </div>
+        <button style={{fontSize: '1.5rem', margin: '1.5rem 0 3rem 0.5rem'}}>Get Coin Info</button>
+        <button onClick={() => setHideAddCoin(true)} style={{display: 'block', fontSize: '1.5rem', margin: '1.5rem 0 1.5rem 0.5rem'}}>Add New Coin</button>
+        <div style={{display: hideAddCoin ? 'flex' : 'none', flexFlow: 'column nowrap', width: '25rem'}}>
+          <label style={{fontSize: '1.5rem', margin: '0.5rem 0 0.5rem 0.5rem'}} htmlFor='nameField'>Name of Coin</label>
+          <input onChange={handleInputChange} type='text' id='nameField'></input>
+          <label style={{fontSize: '1.5rem', margin: '1.5rem 0 0.5rem 0.5rem'}} htmlFor='dateField'>Name of Date Field</label>
+          <input onChange={handleInputChange} type='text' id='dateField'></input>
+          <label htmlFor='isUnix' style={{fontSize: '1.5rem', margin: '0.5rem 0 0.5rem 0'}}>Is This a Unix Date Field</label>
+          <select onChange={handleInputChange} style={{width: '40%', fontSize: '1.4rem', margin: '0 0 2rem 0'}} name='isUnix' id='isUnix'>
+            <option value='no'>No</option>
+            <option value='yes'>Yes</option>
+          </select>
+          <label style={{fontSize: '1.5rem', margin: '0.5rem 0 0.5rem 0.5rem'}} htmlFor='amountField'>Name of Coin Amount Field</label>
+          <input onChange={handleInputChange} type='text' id='amountField'></input>
+          <button style={{width: '60%', fontSize: '1.5rem', margin: '3rem 0 0.5rem 0.5rem'}}>Submit New Coin</button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ server/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/crypto', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.error('connection error')
+});
+db.once('open', () => {
+  console.log('Connected to Mongo!')
+});
+
+const coinSchema = mongoose.Schema({
+  coin: String,
+  dateField: String,
+  isUnix: String,
+  amountField: String
+});
+
+const Coin = mongoose.model('Coin', coinSchema);
+
+module.exports = Coin;
